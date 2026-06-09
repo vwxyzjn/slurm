8184693053d7d97e8ddaa6bc216c449e9c2f3c72
Costa Huang <costa@periodiclabs.ai>
2026-06-09 16:54:54 -0700

    Add hetjob launch transactions

--- src/slurmctld/slurmctld.h
+++ src/slurmctld/slurmctld.h
@@ -309,6 +309,8 @@ extern bitstr_t *avail_node_bitmap;	/* bitmap of available nodes,
 					 * state not DOWN, DRAIN or FAILING */
 extern bitstr_t *bf_ignore_node_bitmap;	/* bitmap of nodes made available during
 					 * backfill cycle */
+extern bitstr_t *het_job_launch_node_bitmap; /* nodes held by active hetjob
+					      * launch transactions */
 extern bitstr_t *booting_node_bitmap;	/* bitmap of booting nodes */
 extern bitstr_t *cg_node_bitmap;	/* bitmap of completing nodes */
 extern bitstr_t *cloud_node_bitmap;	/* bitmap of cloud nodes */
